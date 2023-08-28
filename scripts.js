9b227a0f0e9586a3bc6f3bfc3c3b9115a17433f6
VictorCabanillas <vector.uno4@gmail.com>
2023-08-28 03:47:26 +0200

    NavBar style changes

--- scripts.js
+++ scripts.js
@@ -1,17 +1,14 @@
-window.onscroll = function() { stickyNavbar() }; 
-window.onload = function () {stickyNavbar()};
-const navbar = document.getElementById("navigationBar"); 
-const sticky = navbar.offsetTop; 
+window.onscroll = function () { stickyNavbar() };
+window.onload = function () { stickyNavbar() };
+const navbar = document.getElementById("navigationBar");
+const sticky = navbar.offsetTop;
 
-function stickyNavbar() 
-{ 
-if (window.scrollY >= sticky) 
-{ 
-navbar.classList.add("sticky"); 
-} 
-else 
-{ 
-navbar.classList.remove("sticky"); 
-console.log("Removed sticky");
-} 
+function stickyNavbar() {
+    if (window.scrollY >= sticky) {
+        navbar.classList.add("sticky");
+    }
+    else {
+        navbar.classList.remove("sticky");
+        console.log("Removed sticky");
+    }
 }
